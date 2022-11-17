e0c035dde931b5150c429a527092e79a9967e7fc
jitesh singh <jiteshjitsun@gmail.com>
2022-11-18 00:06:19 +0530

    added models for amenity facility roomtype

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
+@admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
+
+class ItemAdmin(admin.ModelAdmin):
+    pass
+
 @admin.register(models.Room)
 
 class RoomAdmin(admin.ModelAdmin):  
--- rooms/models.py
+++ rooms/models.py
@@ -4,6 +4,35 @@ from users import models as user_models
 from django_countries.fields import CountryField
 
 # Create your models here.
+
+class AbstractItem(core_models.TimeStampedModel):
+    """ abstract items """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True 
+    
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+
+    pass
+
+class Amenity(AbstractItem):
+
+    pass
+
+class Facility(AbstractItem):
+    """ facility model defination """
+    pass
+
+class HouseRule(AbstractItem):
+    """ House rule model """
+    pass
+
 class Room(core_models.TimeStampedModel):
     # rooms model defination
     name = models.CharField(max_length=200, default="")
@@ -17,3 +46,11 @@ class Room(core_models.TimeStampedModel):
     baths = models.IntegerField(default=0)
     instant_book = models.BooleanField(default=False)
     owner = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField(Amenity)
+    house_rules = models.ManyToManyField(HouseRule)
+    
+
+    def __str__(self):
+        return self.name
+    
