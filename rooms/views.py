e160c4e85168d91d03d1ad2267aa93a24b6a25bd
jitesh singh <jiteshjitsun@gmail.com>
2022-12-02 22:00:38 +0530

    all mighty push

--- rooms/views.py
+++ rooms/views.py
@@ -1,11 +1,24 @@
+from math import ceil
 from django.shortcuts import render
-from datetime import datetime
+from . import models
 
 # Create your views here.
 
 
 def all_rooms(request):
-    now = datetime.now()
-    return render(request, "all_rooms.html", context={
-        'now':now,
+    page = request.GET.get("page", 1)
+    if page == "":
+        page = 1
+    else:
+        page = int(page)
+    page_size = 10
+    limit = page_size * page
+    offset = limit - page_size
+    all_rooms = models.Room.objects.all()[offset:limit]
+    page_count = ceil(models.Room.objects.count() / page_size)
+    return render(request, "rooms/home.html", context={
+        "rooms": all_rooms,
+        "page": page,
+        "page_count": page_count,
+        "page_range": range(1, page_count),
     })
