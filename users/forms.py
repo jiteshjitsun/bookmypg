2daea1f48f3596c6f5f11364df2e4e552582a481
jitesh singh <jiteshjitsun@gmail.com>
2022-12-10 11:48:39 +0530

    get set , pack your bag we movin out login hogaya

--- users/forms.py
+++ users/forms.py
@@ -1,19 +1,28 @@
 from django import forms
 from . import models
 
+
 class LoginForm(forms.Form):
 
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
 
-    def clean_email(self):
-        email = self.cleaned_data.get("email")
-        try:
-            models.User.objects.get(username=email)
-            return email
-        except models.User.DoesNotExist:
-            raise forms.ValidationError("User does not exist")
-        print("clean email")
+    # def clean_email(self):
+    #     email = self.cleaned_data.get("email")
+    #     try:
+    #         models.User.objects.get(username=email)
+    #         return email
+    #     except models.User.DoesNotExist:
+    #         raise forms.ValidationError("User does not exist")
 
-    def clean_password(self):
-        print("clean pw")
+    def clean(self):
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        try:
+            user = models.User.objects.get(email=email)
+            if user.check_password(password):
+                return self.cleaned_data
+            else:
+                self.add_error("password", forms.ValidationError("Wrong Password"))
+        except models.User.DoesNotExist:
+            self.add_error("email", forms.ValidationError("user does not exist"))
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.views import View
 from . import forms
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -15,6 +16,10 @@ class LoginView(View):
         form = forms.LoginForm(request.POST)
         if form.is_valid():
             print(form.cleaned_data)
-        return render(request, "users/login.html", {"form":form})
-
-
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(reverse("core:home"))
+        return render(request, "users/login.html", {"form": form})
