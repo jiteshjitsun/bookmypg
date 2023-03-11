71ebabaa9451796b7838bd2cc96107086f9cd38c
jitesh singh <jiteshjitsun@gmail.com>
2023-03-12 02:07:39 +0530

    css sucks but fun to play with

--- rooms/views.py
+++ rooms/views.py
@@ -53,7 +53,7 @@ class HomeView(ListView):
     """ HomeView definition """
 
     model = models.Room
-    paginate_by = 10
+    paginate_by = 12
     paginate_orphans = 5
     ordering = "created"
     context_object_name = "rooms"
