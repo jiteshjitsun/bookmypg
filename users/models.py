59d519a03887399b384771d6873e305b8e8fd1b5
jitesh singh <jiteshjitsun@gmail.com>
2023-03-13 10:46:43 +0530

    little bit fu***d signup form :) shit here we go again

--- users/models.py
+++ users/models.py
@@ -5,7 +5,9 @@ from django.contrib.auth .models import AbstractUser
 from django.core.mail import send_mail
 from django.utils.html import strip_tags
 from django.template.loader import render_to_string
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 # Create your models here.
 geneder_choices = (
     ("male", "male"),
